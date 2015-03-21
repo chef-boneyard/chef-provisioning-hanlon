b36f7a0bcf1f3366b8ec0d8acbf435c47d66053a
Chris McClimans <chris@hippiehacker.org>
2015-03-21 00:16:08 -0700

    non-working tagrule... api update needed

---
+++ lib/chef/resource/hanlon_tagrule.rb
@@ -0,0 +1,27 @@
+require 'chef/provisioning'
+
+class Chef::Resource::HanlonTagrule < Chef::Resource::LWRPBase
+  self.resource_name = 'hanlon_tagrule'
+
+  #def initialize(driver_url, config)
+  #  super
+  #  @driver = run_context.chef_provisioning.current_driver
+  #end
+
+  actions :create, :delete, :nothing #, :delete # forthcoming
+  default_action :create
+
+  attribute :driver
+  attribute :name, kind_of: String, name_attribute: true, required: true
+  attribute :tag, required: true
+  attribute :match, required: true, kind_of: Array
+  # matches should have some validation, but this is just to get to an mvp
+  def after(&block)
+    block ? @after = block : @after
+  end
+
+  # Are we interested in Chef's cloning behavior here?
+  def load_prior_resource(*args)
+    Chef::Log.debug("Overloading #{resource_name}.load_prior_resource with NOOP")
+  end
+end
